2845e414d44879ec3d9fcda4e72d0e1a7553a847
janitha000 <JanithaT@99x.lk>
2019-04-02 10:51:34 +0530

    Added RepositoryBase class

--- Repository/Generic/Repositorybase.cs
+++ Repository/Generic/Repositorybase.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using React.Entity;
+using React.Repository.Context;
 
 namespace React.Repository.Generic
 {
@@ -9,19 +13,55 @@ namespace React.Repository.Generic
     {
         private DatabaseContext _context;
 
+        public Repositorybase(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public virtual void Add(T entity)
+        {
+            EntityEntry dbEntityEntry = _context.Entry<T>(entity);
+            _context.Set<T>().Add(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            EntityEntry dbEntityEntry = _context.Entry<T>(entity);
+            dbEntityEntry.State = EntityState.Deleted;
+        }
+
+        public void DeleteWhere(Expression<Func<T, bool>> predicate)
+        {
+           IEnumerable<T> entities = _context.Set<T>().Where(predicate);
+           foreach(var entity in entities){
+               _context.Entry<T>(entity).State = EntityState.Deleted;
+           }
+        }
+
+        public IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().Where(predicate);
+        }
+
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().AsEnumerable();
         }
 
         public T GetSingle(string id)
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().FirstOrDefault(x => x.Id == id);
         }
 
         public T GetSingle(Expression<Func<T, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public void Update(T entity)
+        {
+            EntityEntry dbEntityEntry = _context.Entry<T>(entity);
+            dbEntityEntry.State = EntityState.Modified;
         }
     }
 }
--- Repository/Interfaces/IRepositoryBase.cs
+++ Repository/Interfaces/IRepositoryBase.cs
@@ -10,6 +10,12 @@ namespace React.Repository
         IEnumerable<T> GetAll();
         T GetSingle(string id);
         T GetSingle(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete (T entity);
+        void DeleteWhere(Expression<Func<T, bool>> predicate);
+
         
     }
 }
