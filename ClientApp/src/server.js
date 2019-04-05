28df8437a1a25cc533dbd8be8d6fc03bd159128d
janitha000 <JanithaT@99x.lk>
2019-04-05 16:29:53 +0530

    Added react form

--- ClientApp/src/server.js
+++ ClientApp/src/server.js
@@ -2,37 +2,42 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import Table from './components/Table';
+import Form from './components/Form';
 
 export default class Server extends React.Component {
     state = {
-        users : [
+        users: [
             {
-              name: 'Charlie',
-              job: 'Janitor',
+                name: 'Charlie',
+                job: 'Janitor',
             },
             {
-              name: 'Mac',
-              job: 'Bouncer',
+                name: 'Mac',
+                job: 'Bouncer',
             },
             {
-              name: 'Dee',
-              job: 'Aspring actress',
+                name: 'Dee',
+                job: 'Aspring actress',
             },
             {
-              name: 'Dennis',
-              job: 'Bartender',
+                name: 'Dennis',
+                job: 'Bartender',
             },
-          ],
+        ],
     };
 
     removeUser = index => {
         const { users } = this.state;
 
         this.setState({
-            users : users.filter((user, i) => {
+            users: users.filter((user, i) => {
                 return i !== index
             })
         })
+    }
+
+    handleSubmit = user => {
+        this.setState({ users : [this.state.users, user]});
     }
 
     render() {
@@ -40,7 +45,10 @@ export default class Server extends React.Component {
             [
                 <div className="container"><h1> Sample application</h1></div>,
                 <div className="container">
-                    <Table userData = {this.state.users} removeUser = {this.removeUser} />
+                    <Table userData={this.state.users} removeUser={this.removeUser} />
+                </div>,
+                <div className="container">
+                    <Form handleSubmit ={this.handleSubmit} />
                 </div>
 
             ]
